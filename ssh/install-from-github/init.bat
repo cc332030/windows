a1f7d26e7ccdccbb63367abedb98fb197abd6947
c332030 <c332030@gmail.com>
2023-03-08 17:30:31 +0800

    feat: ssh

--- ssh/install-from-github/init.bat
+++ ssh/install-from-github/init.bat
@@ -3,8 +3,14 @@
 set SSH_PROGRAM_PATH=C:\Program Files\OpenSSH
 
 call link-dir %~dp0app "%SSH_PROGRAM_PATH%"
+::call link-dir %~dp0config "%programdata%\ssh"
+
+net stop sshd
 
 cd /d "%SSH_PROGRAM_PATH%"
+powershell.exe -ExecutionPolicy Bypass -File uninstall-sshd.ps1
 powershell.exe -ExecutionPolicy Bypass -File install-sshd.ps1
+
+net start sshd
 
 pause >nul
