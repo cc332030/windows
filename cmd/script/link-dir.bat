c245a6d18813d9b2f6c4fb68fdb613fa7a503abc
c332030 <c332030@c332030.com>
2023-03-18 22:06:56 +0800

    feat: remove space

--- cmd/script/link-dir.bat
+++ cmd/script/link-dir.bat
@@ -1,36 +1,36 @@
 @echo off
 
-set source="%1"
-set target="%2"
+set source=%1
+set target=%2
 
 if not defined source (
-  set existSource="false"
+  set existSource=false
   echo.
   set /p source=请输入源目录：
 ) else (
-  set existSource="true"
+  set existSource=true
 )
 
 if not defined target (
-  set existTarget="false"
+  set existTarget=false
   echo.
   set /p target=请输入目标目录：
 ) else (
-  set existTarget="true"
+  set existTarget=true
 )
 
-if not exist "%source%" (
-  mkdir "%source%"
+if not exist %source% (
+  mkdir %source%
 )
 
 :: 创建父级目录
-if not exist "%target%" (
-  mkdir "%target%"
+if not exist %target% (
+  mkdir %target%
 )
-rd /s /q "%target%"
+rd /s /q %target%
 
 echo.
-mklink /d "%target%" "%source%"
+mklink /d %target% %source%
 
 set skipParse=true
 
--- cmd/script/link-file.bat
+++ cmd/script/link-file.bat
@@ -1,55 +1,55 @@
-@echo off
+::@echo off
 
-set source="%1"
-set target="%2"
+set source=%1
+set target=%2
 
-set sourceFileName="%3"
-set targetFileName="%4"
+set sourceFileName=%3
+set targetFileName=%4
 
 if not defined targetFileName (
-  set targetFileName="%sourceFileName%"
+  set targetFileName=%sourceFileName%
 )
 
 if not defined targetFileName (
-  set existTargetFileName="false"
+  set existTargetFileName=false
   echo.
   set /p sourceFileName=请输入源文件名：
-  set targetFileName="%sourceFileName%"
+  set targetFileName=%sourceFileName%
 ) else (
-  set existTargetFileName="true"
+  set existTargetFileName=true
 )
 
 if not defined source (
-  set existSource="false"
+  set existSource=false
   echo.
   set /p source=请输入源文件路径：
 ) else (
-  set existSource="true"
+  set existSource=true
 )
 
 if not defined target (
-  set existTarget="false"
+  set existTarget=false
   echo.
   set /p target=请输入目标文件路径：
 ) else (
-  set existTarget="true"
+  set existTarget=true
 )
 
-if not exist "%source%" (
-  mkdir "%source%"
+if not exist %source% (
+  mkdir %source%
 )
 
-if not exist "%source%\%sourceFileName%" (
+if not exist %source%\%sourceFileName% (
   echo.
-  echo 不存在文件 "%source%\%sourceFileName%"
+  echo 不存在文件 %source%\%sourceFileName%
   pause >nul
   exit 0
 )
 
-if exist "%target%\%targetFileName%" (
-  del /f /q "%target%\%targetFileName%"
-) else if not exist "%target%" (
-  mkdir "%target%"
+if exist %target%\%targetFileName% (
+  del /f /q %target%\%targetFileName%
+) else if not exist %target% (
+  mkdir %target%
 )
 
 :: 相同磁盘下使用硬链接
@@ -58,7 +58,7 @@ if exist "%target%\%targetFileName%" (
 :: )
 
 echo.
-mklink "%target%\%targetFileName%" "%source%\%sourceFileName%"
+mklink %target%\%targetFileName% %source%\%sourceFileName%
 
 set skipParse=true
 
